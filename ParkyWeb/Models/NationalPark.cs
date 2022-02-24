9c1ca2677aa2de50a66064f5159df0d3d3c1a159
Renuka Patil <69632043+renukapatil-21@users.noreply.github.com>
2022-02-24 14:35:18 +0530

    Update NationalPark.cs

--- ParkyWeb/Models/NationalPark.cs
+++ ParkyWeb/Models/NationalPark.cs
@@ -13,7 +13,7 @@ namespace ParkyWeb.Models
         public string Name { get; set; }
         [Required]
         public string State { get; set; }
-        public byte[] Picture { get; set; }
+        public byte[]? Picture { get; set; }
         public DateTime Created { get; set; }
         public DateTime Established { get; set; }
     }
