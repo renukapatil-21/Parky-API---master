af8b00abdef0e4a4ae1f8c78898af335509efa62
Renuka Patil <69632043+renukapatil-21@users.noreply.github.com>
2022-02-24 11:21:36 +0530

    Delete ParkyWeb/Models directory

--- ParkyWeb/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ParkyWeb.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
